3031ca6f432bf4c5fad79aa518404fc376d9dd9f
jaggernaut007 <shreyasjagannath08@gmail.om>
2019-08-15 00:14:57 +0530

    git ignore edit 2

--- qlearning.py
+++ qlearning.py
@@ -69,7 +69,6 @@ for episode in range(EPISODES):
 
 	ep_rewards.append(episode_reward)
 	if not episode % SHOW_EVERY:
-		print(len(ep_rewards[-SHOW_EVERY:]),len(ep_rewards[SHOW_EVERY:]))
 		average_reward = sum(ep_rewards[-SHOW_EVERY:])/len(ep_rewards[-SHOW_EVERY:])
 		aggr_ep_rewards['ep'].append(episode)
 		aggr_ep_rewards['avg'].append(average_reward)
